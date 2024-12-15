7e32cf6f10b0bcb75c344e9921d74a6928728c11
DESKTOP-MF8V5D1\Andrey <m>
2024-12-15 14:01:18 +0300

    feat: 18 passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -374,8 +374,27 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+
+function sortByAsc(arr) {
+  if (arr.length <= 1) return arr;
+  const result = arr;
+  const pivot = arr[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i += 1) {
+    if (arr[i] < pivot) {
+      left[left.length] = arr[i];
+    } else {
+      right[right.length] = arr[i];
+    }
+  }
+  left = sortByAsc(left);
+  right = sortByAsc(right);
+  const preSort = [...left, pivot, ...right];
+  for (let i = 0; i < arr.length; i += 1) {
+    result[i] = preSort[i];
+  }
+  return result;
 }
 
 /**
